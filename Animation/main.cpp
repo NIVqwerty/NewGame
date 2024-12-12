abc02733162761c26d560a23aa68fa9338ee31e1
NIVqwerty <igoreknesh@gmail.com>
2024-12-12 19:18:53 +0400

    Add files via upload

--- Animation/main.cpp
+++ Animation/main.cpp
@@ -55,13 +55,30 @@ struct Mario
     }
 
 };
+struct Barrier
+{
+    int x;
+    int y;
+
+    void draw()
+    {
+        txSetColor(TX_BLACK);
+        txSetFillColor(TX_RED);
+        txRectangle(x,y,x+20,y+50);
+    }
+
+} ;
 
 int main()
 {
 txCreateWindow (800, 600);
 txTextCursor (false);
+Barrier bar[2];
+ bar[0] = {200,50};
+ bar[1] = {600,50};
 
 Mario mario = {0, 300, 5, txLoadImage("Pictures/mario_r.bmp"), txLoadImage("Pictures/mario_l.bmp"), txLoadImage("Pictures/mario_r.bmp"), 0, 0};
+//Массив
 Mario mario_bot[5];
  mario_bot[0] = {100, 250, 2, mario.image, mario.image_l, mario.image_r, 0, 0};
  mario_bot[1] = {200, 200, 3, mario.image, mario.image_l, mario.image_r, 0, 0};
@@ -73,9 +90,14 @@ Mario mario_bot[5];
     {
         txClear();
         txBegin();
-
+        for(int i=0; i<2; i++)
+        {
+        bar[0].draw();
+        bar[1].draw();
+        }
+        txSetFillColor(TX_WHITE);
+    //главный герой
         mario.draw();
-
         if (GetAsyncKeyState(VK_RIGHT))
         {
             mario.x += mario.vx;
@@ -88,13 +110,14 @@ Mario mario_bot[5];
             mario.image = mario.image_l;
             mario.animation();
         }
-
-        for (int i=0; i<5; i++)
+    //------------------------
+    //Боты
+        for (int i=0; i<4; i++)
         {
         mario_bot[i].draw();
         mario_bot[i].animation();
         mario_bot[i].x += mario_bot[i].vx;
-        if (mario_bot[i].x>800-84)
+        if (mario_bot[i].x>800-84 )
         {
             mario_bot[i].vx = -mario_bot[i].vx;
             mario_bot[i].image = mario_bot[i].image_l;
@@ -105,9 +128,25 @@ Mario mario_bot[5];
             mario_bot[i].image = mario_bot[i].image_r;
         }
         }
+        mario_bot[4].draw();
+        mario_bot[4].animation();
+        mario_bot[4].x += mario_bot[4].vx;
+
+         if (mario_bot[4].x+20>bar[1].x )
+        {
+            mario_bot[4].vx = -mario_bot[4 ].vx;
+            mario_bot[4].image = mario_bot[4].image_l;
+        }
+         if (mario_bot[4].x<bar[0].x  )
+        {
+            mario_bot[4].vx = -mario_bot[4 ].vx;
+            mario_bot[4].image = mario_bot[4].image_r;
+
+        }
         txEnd();
         txSleep(30);
     }
+    //------------------------
 
 txDeleteDC(mario.image);
 txDeleteDC(mario.image_r);
